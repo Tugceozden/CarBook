1c562a79bf6c9015fcf04ddfad095efa6c9dec7f
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-11-09 22:36:08 +0300

    Add project files.

---
+++ Core/UdemyCarBookDomain/Entities/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBookDomain.Entities
+{
+	public class Location
+	{
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+
+
+
+    }
+}
