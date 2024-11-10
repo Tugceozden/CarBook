2c764bbc11f9b36302cdb82c77f8da701fb038ab
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-11-10 20:28:05 +0300

    defining entities added

---
+++ Core/UdemyCarBookDomain/Entities/FooterAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBookDomain.Entities
+{
+	public class FooterAddress
+	{
+        public int FooterAddressIds { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+
+
+    }
+}
