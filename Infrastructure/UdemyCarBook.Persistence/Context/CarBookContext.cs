8eab5bf3faabe43d2138393a532e109e2880b961
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-11-11 21:11:46 +0300

    added new project

---
+++ Infrastructure/UdemyCarBook.Persistence/Context/CarBookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Persistence.Context
+{
+	public class CarBookContext
+	{
+        public int CarBookContextId { get; set; }
+
+
+
+
+
+    }
+}
