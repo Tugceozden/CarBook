4207d71a68dfda8c5e461028b515b9bfd5ecd99d
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-11-12 18:24:38 +0300

    repository class created

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Interfaces;
+
+namespace UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+	public class GetAboutQueryHandler
+	{
+		private readonly IRepository<About>
+
+
+
+
+
+	}
+}
