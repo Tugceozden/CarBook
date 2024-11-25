b6a7ee0753c58de3efec536e69afc520bc00018f
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-11-25 20:41:11 +0300

    New files added to the project

--- Presentation/UdemyCarBook.WebApi/Program.cs
+++ Presentation/UdemyCarBook.WebApi/Program.cs
@@ -1,6 +1,14 @@
+using UdemyCarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<GetAboutQueryHandler>();	
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();	
+builder.Services.AddScoped<CreateAboutCommandHandler>();	
+builder.Services.AddScoped<UpdateAboutCommandHandler>();	
+builder.Services.AddScoped<RemoveAboutCommandHandler>();	
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
