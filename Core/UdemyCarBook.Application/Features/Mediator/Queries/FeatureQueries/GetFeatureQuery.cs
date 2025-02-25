7642b06313381e62b93d75171f2f2a53c7787dfa
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-02-25 12:31:29 +0300

    Added MediatR

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Queries/FeatureQueries/GetFeatureQuery.cs
@@ -0,0 +1,11 @@
+﻿
+
+using MediatR;
+using UdemyCarBook.Application.Features.Mediator.Results.FetureResults;
+
+namespace UdemyCarBook.Application.Features.Mediator.Queries.FeatureQueries
+{
+	public class GetFeatureQuery:IRequest<List<GetFeatureQueryResult>>
+	{
+	}
+}
---
+++ Core/UdemyCarBook.Application/Features/Mediator/Results/FeatureResults/GetFeatureQueryResult.cs
@@ -0,0 +1,8 @@
+﻿namespace UdemyCarBook.Application.Features.Mediator.Results.FetureResults
+{
+    public class GetFeatureQueryResult
+    {
+        public int FeatureId { get; set; }
+        public string Name { get; set; }
+    }
+}
