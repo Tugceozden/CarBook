2452bd38cfa38f599b666646d52884389f7d3bf2
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-01 11:03:41 +0300

    Social Media APİ

--- Core/UdemyCarBook.Application/Features/Mediator/Commands/SocialMediaCommands/CreateSocialMediaCommand.cs
+++ Core/UdemyCarBook.Application/Features/Mediator/Commands/SocialMediaCommands/CreateSocialMediaCommand.cs
@@ -7,7 +7,7 @@ namespace UdemyCarBook.Application.Features.Mediator.Commands.SocialMediaCommand
 	public class CreateSocialMediaCommand:IRequest
 	{
 		public string Name { get; set; }
-		public string Icon { get; set; }
-		public string Url { get; set; }
+		public string? Icon { get; set; }
+		public string? Url { get; set; }
 	}
 }
--- Core/UdemyCarBookDomain/Entities/SocialMedia.cs
+++ Core/UdemyCarBookDomain/Entities/SocialMedia.cs
@@ -10,8 +10,8 @@ namespace UdemyCarBookDomain.Entities
 	{
         public int SocialMediaId { get; set; }
         public string Name { get; set; }
-        public string Icon { get; set; }
-        public string Url { get; set; }
+        public string? Icon { get; set; } 
+		public string? Url { get; set; }
 
 
 
