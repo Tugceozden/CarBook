20c57b2ee52b95caeb81adfd819e4e0e161e3386
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-02 16:24:30 +0300

    Partial added

---
+++ Frontends/CarBookWebUI/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invooke()
+        {
+            return View();  
+        }
+    }
+}
