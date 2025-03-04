72185e3da99e3cddfffa1591b3e459089ae4c818
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-04 13:59:54 +0300

    Required Partial added

---
+++ Frontends/CarBookWebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookWebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IViewComponentResult> InvokeAsync()
+        {
+            var client= _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("");
+         if(responseMessage.IsSuccessStatusCode) 
+            {
+
+                var JsonData= await responseMessage.Content.ReadAsStringAsync();  
+                var values=JsonConvert.DeserializeObject<List<string>>(JsonData);   
+                return View(values);
+
+            }
+         return View();
+        }
+    }
+}
