32630c69b7ab0154d449e2fbab970e7e66880ffe
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-04 11:53:09 +0300

    Dto added

--- Frontends/CarBookWebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
+++ Frontends/CarBookWebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.AboutDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace CarBookWebUI.ViewComponents.AboutViewComponents
-{
+namespace CarBookWebUI.ViewComponents.AboutViewComponents;
     public class _AboutUsComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() 
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync() 
         { 
+        var client= _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7234/api/Abouts");
+        if(responseMessage.IsSuccessStatusCode)
+        {
+            var JsonData= await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(JsonData);
+            return View(values);
+        }
             return View(); 
         } 
     }
-}
+
