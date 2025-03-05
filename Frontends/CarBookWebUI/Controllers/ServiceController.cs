69a0cd867369099c76eaf2443cd9e91fd482e93f
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-05 16:31:30 +0300

    Service added

---
+++ Frontends/CarBookWebUI/Controllers/ServiceController.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookWebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task< IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7012/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(JsonData);
+                return View(values);
+
+            }
+            return View(new List<ResultServiceDto>());
+        }
+    }
+}
