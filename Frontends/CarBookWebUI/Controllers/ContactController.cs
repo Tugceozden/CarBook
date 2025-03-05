7ca33fd932ff6d1b124bd2a3b4ea5784d587d3c4
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-05 22:26:44 +0300

    Contact added

--- Frontends/CarBookWebUI/Controllers/ContactController.cs
+++ Frontends/CarBookWebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBookWebUI.Controllers
 {
@@ -10,10 +13,25 @@ namespace CarBookWebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]   
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client=_httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7012/api/Contacts",stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+
+            }
+            return View();  
         }
     }
 }
