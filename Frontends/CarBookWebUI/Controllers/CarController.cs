e3e8616cc70c9c7c07d8d08230112e0c92b1690b
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-05 17:12:40 +0300

    Car added

---
+++ Frontends/CarBookWebUI/Controllers/CarController.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookWebUI.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7012/api/Cars/GetCarWhithBrand");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(JsonData);
+                return View(values);
+
+            }
+            return View(new List<ResultCarWithBrandDto>());
+        }
+    }
+}
