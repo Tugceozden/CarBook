2ab8f894ec6b606fd9324d52995ec9fb0b9127ed
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-06 13:10:45 +0300

    Homepage added

---
+++ Frontends/CarBookWebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
@@ -0,0 +1,32 @@
+﻿using CarBook.Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBookWebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCoverUILayoutComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultCoverUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7012/api/Banners");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(JsonData);
+                return View(values);
+
+            }
+            return View(new List<ResultBannerDto>());
+        }
+    }
+}
