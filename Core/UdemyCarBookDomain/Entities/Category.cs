117356448d31530f9c1b936fcd03d723ec6c0961
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-03-12 17:11:00 +0300

    Added Author and Blog entity

--- Core/UdemyCarBookDomain/Entities/Category.cs
+++ Core/UdemyCarBookDomain/Entities/Category.cs
@@ -11,7 +11,7 @@ namespace UdemyCarBookDomain.Entities
         public int CategoryId { get; set; }
         public string Name { get; set; }
 
-
+        public List<Blog> Blogs { get; set; }
 
 
     }
